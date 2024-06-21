0390381a057f83027cacc421d55d68c3b9f1f3ef
Ubuntu <ubuntu@ip-172-31-39-61.ap-south-1.compute.internal>
2024-06-21 04:19:44 +0000

    dev

--- internal/controller/selfhostedstage_controller.go
+++ internal/controller/selfhostedstage_controller.go
@@ -40,9 +40,10 @@ type SelfhostedstageReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=selfhosted.dev.my.domain,resources=selfhostedstages,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=selfhosted.dev.my.domain,resources=selfhostedstages/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=selfhosted.dev.my.domain,resources=selfhostedstages/finalizers,verbs=update
+// +kubebuilder:rbac:groups=selfhosted.dev.my.domain,resources=selfhostedstages,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=selfhosted.dev.my.domain,resources=selfhostedstages/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=selfhosted.dev.my.domain,resources=selfhostedstages/finalizers,verbs=update
+// +kubebuilder:rbac:groups="",resources=namespaces,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
